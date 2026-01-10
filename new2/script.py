8c6e58bc337ca4bac7d7c0ed5c143ac5f7346f09
Nihat Saltabas <saltabasnihat@gmail.com>
2026-01-09 16:35:30 -0800

    updated .py file

--- new2/script.py
+++ new2/script.py
@@ -1,1 +1,10 @@
-print("Hello World")
+import random
+
+object1 = "Eren"
+
+x = random.randint(0,20)
+
+if x >10:
+    print("Hello " + object1)
+else:
+    print("Sorry, you are not welcomed "+ object1)
